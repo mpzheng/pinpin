ed38ad2b9cbfb5a8e94ef956a67f37d4d06c28ff
方道友 <54983082+fangdaoyou@users.noreply.github.com>
2019-11-16 20:27:15 +0800

    Update pinFriend.js

--- pinFriend/pinFriend.js
+++ pinFriend/pinFriend.js
@@ -1,8 +1,21 @@
 Page({
-
-  
   data: {
-
+    listItem: [
+      {
+      name:'Van',
+      sdept:'人文学院',
+      tel:'13616071248',
+      sex:'男'
+      },
+      {
+        name: 'Bili',
+        sdept: '人文学院',
+        tel: '13616071298',
+        sex: '男'
+      },
+      {},
+      {}
+    ]
   },
 
   
