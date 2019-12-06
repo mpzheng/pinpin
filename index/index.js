4150d3385dae57ab32b03909d994ae623b57b8c3
mpzheng <52969917+mpzheng@users.noreply.github.com>
2019-12-06 21:37:13 +0800

    Update index.js

--- index/index.js
+++ index/index.js
@@ -10,7 +10,7 @@ Page({
   getAim: function(e) {
     this.setData({
       tempAim: e.detail.value,
-      aimName: e.detail.value
+      aimName: e.detail.value,
     })
   },
   sendAim: function(e) {
